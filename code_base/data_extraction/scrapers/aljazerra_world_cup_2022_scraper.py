4dee94329e905e69af03dae90f695813424689da
Abdulnaser98 <abdalnaser66685@gmail.com>
2023-03-15 19:26:47 +0100

    code refactor

--- code_base/data_extraction/scrapers/aljazerra_world_cup_2022_scraper.py
+++ code_base/data_extraction/scrapers/aljazerra_world_cup_2022_scraper.py
@@ -12,16 +12,15 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
 # Path to the raw data folder
-path_to_raw_data_folder = "/Users/abdulnaser/Desktop/DHKatar/code_base/data_extraction/raw_data/"
+path_to_raw_data_folder = "./code_base/data_extraction/raw_data/"
 
 # Path to the folder , where the links that were extracted from the news media outlets are saved
-path_to_article_links_folder = "/Users/abdulnaser/Desktop/DHKatar/code_base/data_extraction/article_links/"
+path_to_article_links_folder = "./code_base/data_extraction/article_links/"
 
 # contains the links of the bbc news regarding the qatar world cup 2022
-article_links = []
 article_dates = []
 titles = []
-head_title = []
+head_titles = []
 main_contents = []
 
 
@@ -33,11 +32,11 @@ main_content_path = "//main[contains(@id, 'main-content-area')]"
 def aljazerra_web_scraping_main():
 
     # extract the links
-    extract_links()
+    links = extract_links()
     # remove links that are irrelevant to our task , like sound links and etc
-    remove_unwanted_links()
+    links_filtered = remove_unwanted_links(links)
     # extract the relevant article data , like date , title , main content
-    extract_article_data()
+    extract_article_data(links_filtered)
     # save the data in the raw data folder
     save_data()
 
@@ -51,7 +50,7 @@ def extract_links():
 
     # extract featured articles first
     featured_articles_links = driver.find_elements("xpath","//ul[contains(@class, 'featured-articles-list')]/li//a[@href]")
-
+    article_links = []
     print("We have {} fetaured items: ".format(len(featured_articles_links)))
     for e in featured_articles_links:
         article_links.append(e.get_attribute("href"))
@@ -87,35 +86,38 @@ def extract_links():
 
     print("We have collected {} links".format(len(article_links)))
 
+    return article_links
 
 
 
-def remove_unwanted_links():
+
+def remove_unwanted_links(links):
     print("Removing irrelevant links: ")
-    unwanted_links = pd.read_csv("/Users/abdulnaser/Desktop/DHKatar/code_base/data_extraction/unwanted_links/aljazerra_unwanted_links.csv")
+    unwanted_links = pd.read_csv("./code_base/data_extraction/unwanted_links/aljazerra_unwanted_links.csv")
     article_links_temp = []
-    for link in article_links:
+    for link in links:
         if link not in unwanted_links:
             article_links_temp.append(link)
-    article_links =  article_links_temp
     #article_links = [x for x in article_links if x not in unwanted_links]
     print("Irrelevant links are removed")
+    return article_links_temp
 
 
-def extract_article_data():
+def extract_article_data(links):
     print("Extracting the articles data: ")
-    for link in tqdm(article_links):
+    for link in tqdm(links):
+        print(link)
         driver.get(link)
         time.sleep(6)
 
         date = extract_article_date()
         title = extract_article_title()
-        head_title = extract_head_title
+        head_title = extract_head_title()
         main_content = extract_main_content()
 
         article_dates.append(date)
         titles.append(title)
-        head_title.append(head_title)
+        head_titles.append(head_title)
         main_contents.append(main_content)
     print("The data for all relevant articles are extracted succefually ")
 
@@ -150,122 +152,4 @@ def extract_main_content():
 def save_data():
     data = {'date': article_dates,'title': titles,'head_title':head_title,'main_content': main_contents }
     df = pd.DataFrame(data)
-    df.to_csv(path_to_raw_data_folder + "aljazerra_qatar_data.csv", index=False)
-
-
-
-"""
-# Opening the website using the `get` method
-driver.get("https://www.aljazeera.com/qatar-world-cup-2022/")
-
-# Adding a time delay of 3 seconds for the page to load
-time.sleep(3)
-
-links = []
-
-# extract featured articles first
-featured_articles_links = driver.find_elements("xpath","//ul[contains(@class, 'featured-articles-list')]/li//a[@href]")
-
-print("We have {} fetaured items: ".format(len(featured_articles_links)))
-for e in featured_articles_links:
-    links.append(e.get_attribute("href"))
-
-#Adding a time delay of 3 seconds for the next set of articles to load
-time.sleep(3)
-
-counter = 0
-while True:
-    try:
-        # Finding the "Show More" button element using xpath
-        show_more = driver.find_element("xpath", "//button[contains(@class, 'show-more-button big-margin')]")
-        # Clicking the "Show More" button using JavaScript
-        driver.execute_script("arguments[0].click();", show_more)
-        counter = counter +1
-        print(counter)
-        print("Show more button clicked")
-        # Adding a time delay of 2 seconds for the next set of articles to load
-        time.sleep(2)
-
-    except:
-        # If the "Show More" button is no longer present, print a message and break out of the loop
-        print("No more Show more button")
-        break
-
-
-# extract news feed links
-news_feed_links = driver.find_elements("xpath","//section[@id='news-feed-container']/article/div[@class='gc__content']/div[@class='gc__header-wrap']/h3[@class='gc__title']//a[@href]")
-for e in news_feed_links:
-    links.append(e.get_attribute("href"))
-
-
-print("We have collected {} links".format(len(links)))
-
-df = pd.DataFrame(links,columns =['link'])
-
-df.to_csv('aljazerra_articles_new_links.csv')
-
-"""
-
-
-"""
-# read the links
-links = pd.read_csv('/Users/abdulnaser/Desktop/DHKatar/code_base/data_extraction/aljazerra_articles_new_links.csv',sep = ',')
-print("the columns are")
-print(links.columns)
-links = links[~links['link'].str.contains("liveblog")]
-
-
-# Extract the following informations from each article date , author , title , head_title , main_content
-article_dates = []
-authors = []
-titles= []
-head_titles = []
-main_contents = []
-
-unwanted_links = ["https://www.aljazeera.com/program/between-us/2022/12/6/world-cup-inside-qatar", "https://www.aljazeera.com/sports/2022/12/6/which-teams-have-reached-the-world-cup-quarter-finals"
-                  , "https://www.aljazeera.com/features/longform/2022/12/5/zhen-kitchen-a-chinese-familys-restaurant-legacy-in-doha",  "https://www.aljazeera.com/sports/2022/12/3/the-five-most-infamous-world-cup-penalty-shoot-outs",
-                  "https://aljazeera.com/wc2022experience/?utm_source=www.aljazeera.com&utm_medium=website&utm_campaign=ucms", "https://www.aljazeera.com/sports/2022/11/17/football-penalty-kicks-explained", "https://www.aljazeera.com/features/longform/2022/11/7/authentic-desi-food-in-doha-the-story-of-punjab-2",
-                  "https://interactive.aljazeera.com/aje/2022/qatar-football-world-cup-2022-quiz/?utm_source=www.aljazeera.com&utm_medium=website&utm_campaign=ucms","https://www.aljazeera.com/sports/2022/10/19/host-qatars-world-cup-carbon-neutral-claims-under-fire",
-                  "https://www.aljazeera.com/sports/2022/10/11/fifa-world-cup-1930","https://interactive.aljazeera.com/aje/2022/visit-qatar-2022-football-world-cup/?utm_source=aljazeera.com&utm_medium=section_page&utm_campaign=world_cup_2022",
-                  "https://www.aljazeera.com/program/newsfeed/2022/7/19/no-pitch-no-problem-young-senegalese-footballers-train-on-roofs", "https://interactive.aljazeera.com/aje/2021/fifa-arab-cup-2021/index.html?utm_source=www.aljazeera.com&utm_medium=website&utm_campaign=ucms"]
-
-
-for link in tqdm(links['link'][800:]):
-    print("Link")
-    print(link)
-
-    if  link  not in unwanted_links:
-        driver.get(link)
-        # Adding a time delay of 3 seconds for the page to load
-        time.sleep(3)
-        # Extract title
-        header_path = "//main[contains(@id, 'main-content-area')]//header[contains(@class, 'article-header') or contains(@class, 'gallery-header')]"
-        main_content_path = "//main[contains(@id, 'main-content-area')]"
-        title = driver.find_element("xpath", header_path+"/h1").text
-        try:
-            head_title = driver.find_element("xpath",header_path+"//p[contains(@class, 'article__subhead css-1wt8oh6')]/em | //h2[contains(@class,'gallery-header__subhead')]").text
-        except:
-            head_title = ""
-        main_content_element = driver.find_elements("xpath",main_content_path+"//div[contains(@class, 'wysiwyg wysiwyg--all-content css-ibbk12') or contains(@class,'gallery-content wysiwyg wysiwyg--all-content css-ibbk12')]/p")
-        main_content = ""
-        for p in main_content_element:
-            main_content = main_content + " ." + p.text
-        date =  driver.find_element("xpath",main_content_path + "//div[contains(@class, 'article-info-block css-ti04u9') or contains(@class, 'article-info-block')]/div[contains(@class,'article-b-l')]//div[contains(@class,'date-simple css-1yjq2zp')]/span[@class='screen-reader-text']").text
-
-        article_dates.append(date)
-        titles.append(title)
-        head_titles.append(head_title)
-        main_contents.append(main_content)
-
-
-
-data = {'date': article_dates, 'title': titles, 'head_titel': head_titles, 'main_content': main_contents }
-df = pd.DataFrame(data)
-df.to_csv("processed_data_very_new_6.csv", index=False)
-
-"""
-
-
-
-
-
+    df.to_csv(path_to_raw_data_folder + "aljazerra_qatar_data_new.csv", index=False)
--- code_base/data_extraction/scrapers/bbc_world_cup_2022_scraper.py
+++ code_base/data_extraction/scrapers/bbc_world_cup_2022_scraper.py
@@ -11,14 +11,13 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
 # Path to the raw data folder
-path_to_raw_data_folder = "/Users/abdulnaser/Desktop/DHKatar/code_base/data_extraction/raw_data/"
+path_to_raw_data_folder = "./code_base/data_extraction/raw_data/"
 
 # Path to the folder , where the links that were extracted from the news media outlets are saved
-path_to_article_links_folder = "/Users/abdulnaser/Desktop/DHKatar/code_base/data_extraction/article_links/"
+path_to_article_links_folder = "./code_base/data_extraction/article_links/"
 
 
 # contains the links of the bbc news regarding the qatar world cup 2022
-article_links = []
 article_dates = []
 titles = []
 main_contents = []
@@ -27,11 +26,11 @@ main_contents = []
 
 def bbc_web_scraping_main():
     # extract the links
-    extract_links()
+    links = extract_links()
     # remove links that are irrelevant to our task , like sound links and etc
-    remove_unwanted_links()
+    filtered_links = remove_unwanted_links(links)
     # extract the relevant article data , like date , title , main content
-    extract_article_data()
+    extract_article_data(filtered_links)
     # save the data in the raw data folder
     save_data()
 
@@ -40,7 +39,7 @@ def bbc_web_scraping_main():
 
 def extract_links():
     print("Extracting the links: ")
-
+    article_links_temp = []
     url = "https://www.bbc.com/news/topics/cwlw3xz0ze8t?page="
     # there is a total number of 20 pages regarding Qatar in the bbc media outlet
     for page in range(1, 20):
@@ -48,26 +47,27 @@ def extract_links():
         driver.get(url + str(page))
         article_links = driver.find_elements("xpath","//main[contains(@id, 'main-content')]//ul/li/div/div/div/div/div/a[@href]")
         for link in article_links:
-            article_links.append(link.get_attribute("href"))
+            article_links_temp.append(link.get_attribute("href"))
 
     df = pd.DataFrame(article_links,columns =['link'])
     df.to_csv(path_to_article_links_folder + 'bbc_articles_links.csv')
 
 
     print("The links are succefually extracted")
+    return article_links_temp
 
 
-
-def remove_unwanted_links():
+def remove_unwanted_links(links):
     print("Removing irrelevant links: ")
-    unwanted_links = pd.read_csv("/Users/abdulnaser/Desktop/DHKatar/code_base/data_extraction/article_links/bbc_unwanted_links.csv")
-    article_links = [x for x in article_links if x not in unwanted_links]
+    unwanted_links = pd.read_csv("./code_base/data_extraction/bbc_unwanted_links.csv")
+    article_links_temp = [x for x in links if x not in unwanted_links]
     print("Irrelevant links are removed")
+    return article_links_temp
 
 
-def extract_article_data():
+def extract_article_data(links):
     print("Extracting the articles data: ")
-    for link in tqdm(article_links):
+    for link in tqdm(links):
         driver.get(link)
         time.sleep(6)
 
@@ -106,74 +106,3 @@ def save_data():
 
 
 
-"""
-# Instantiating the Chrome browser object using webdriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-article_dates = []
-authors = []
-titles= []
-head_titles = []
-main_contents = []
-# specify the URL of the website you want to scrape
-url = "https://www.bbc.com/news/topics/cwlw3xz0ze8t?page="
-
-# specify the number of pages you want to scrape
-num_pages = 19
-#num_pages + 1
-for page in range(18, 20):
-    print(page)
-    driver.get(url + str(page))
-    article_links = driver.find_elements("xpath","//main[contains(@id, 'main-content')]//ul/li/div/div/div/div/div/a[@href]")
-    for link in article_links:
-        links.append(link.get_attribute("href"))
-
-unwanted_links = ["https://www.bbc.com/news/live/uk-wales-politics-63907455", "https://www.bbc.com/sounds/play/m001fm0l",
-                  "https://www.bbc.com/sport/live/football/63687157","https://www.bbc.com/sounds/play/w3ct312k",
-                  "https://www.bbc.com/sounds/play/w3ct3172","https://www.bbc.com/sounds/play/m0017chj",
-                  "https://www.bbc.com/sounds/play/p0byy9sr","https://www.bbc.com/sounds/play/p0bypw3j",
-                  "https://www.bbc.com/sport/cricket/59590568", "https://www.bbc.com/weather/features/31605227",
-                  "https://www.bbc.com/sounds/play/p0767590", "https://www.bbc.com/news/world-us-canada-39732845"
-                  "https://www.bbc.com/programmes/p065bxrj", "https://www.bbc.com/news/world-us-canada-39732845",
-                  "https://www.bbc.com/sport/football/51072079","https://www.bbc.com/programmes/p065bxrj",
-                  "https://www.bbc.com/programmes/p065bx16", "https://www.bbc.com/sounds/play/p062r06l",
-                  "https://www.bbc.com/sounds/play/b08x2zf5", "https://www.bbc.com/sounds/play/p0577kby",
-                  "https://www.bbc.com/sounds/play/p0558xzg", "https://www.bbc.com/sounds/play/p0550j8m",
-                  "https://www.bbc.com/sounds/play/p0524hr4", "https://www.bbc.com/programmes/p051n6q4",
-                  "https://www.bbc.com/sounds/play/b04jmrnm"
-                ]
-counter = 0
-for link in tqdm(links):
-# | //article/div//p/b | //article/div//p/span
-    if link not in unwanted_links:
-        driver.get(link)
-        counter = counter + 1
-        print("Counter: ")
-        print(counter)
-        time.sleep(3)
-        print("link")
-        print(link)
-        time.sleep(3)
-        title = driver.find_element("xpath", "//article//h1[contains(@id,'main-heading') or contains(@id,'page')]").text
-        date =  driver.find_element("xpath", "//article//time").text
-        main_content_elm = driver.find_elements("xpath", "//article/div//p")
-        main_content = ""
-        for p in main_content_elm:
-            main_content = main_content + " ." + p.text
-
-        article_dates.append(date)
-        titles.append(title)
-        main_contents.append(main_content)
-
-
-
-data = {'date': article_dates, 'title': titles, 'main_content': main_contents }
-df = pd.DataFrame(data)
-df.to_csv("bbc_data__very_neu_5.csv", index=False)
-
-
-
-#all_bbc_data.to_csv("all_bbc_qatar_data.csv")
-
-
-
-"""
